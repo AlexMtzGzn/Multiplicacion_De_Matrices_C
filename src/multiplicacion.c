c6b51dc89c4dc92bf4b38c492b11dca63e1cda3e
AlexMtzGzn <alex13141926@gmail.com>
2024-03-05 22:19:26 -0600

    Asignacion de memoria a la matriz

--- src/multiplicacion.c
+++ src/multiplicacion.c
@@ -8,13 +8,28 @@ int **asigancion_Memoria(int *filas, int *columnas, const char letra)
     {
         printf("\nIngresa las filas de la matriz %c: ", letra);
         scanf("%i", &filas);
+
+        if (*filas < 1)
+            printf("\nIngresa de nuevo las columnas de la matriz %c", letra);
+
     } while (*filas < 1);
 
     do
     {
         printf("\nIngresa las columnas de la matriz %c: ", letra);
         scanf("%i", &columnas);
+
+        if (*filas < 1)
+            printf("\nIngresa de nuevo las columnas de la matriz %c", letra);
+
     } while (*columnas < 2);
+
+    int **matriz = (int **)malloc(*filas * sizeof(int *));
+
+    for (int i = 0; i < *filas; i++)
+        matriz[i] = (int *)malloc(*columnas * sizeof(int));
+    
+    return matriz;
 }
 
 int main(void)
