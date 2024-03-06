f4cbc1e45ae2eb388d2ef029e902d123d87888da
AlexMtzGzn <alex13141926@gmail.com>
2024-03-05 22:14:55 -0600

    Declaracion "do while"
    Validar que las filas y las columnas no sean menor que 1

--- src/multiplicacion.c
+++ src/multiplicacion.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** asigancion_Memoria(int * filas, int * columnas, const char letra){
+int **asigancion_Memoria(int *filas, int *columnas, const char letra)
+{
 
-    printf("\nIngresa las filas de la matriz %c: ",letra);
-    printf("\nIngresa las columnas de la matriz %c: ",letra);
-    
+    do
+    {
+        printf("\nIngresa las filas de la matriz %c: ", letra);
+        scanf("%i", &filas);
+    } while (*filas < 1);
 
+    do
+    {
+        printf("\nIngresa las columnas de la matriz %c: ", letra);
+        scanf("%i", &columnas);
+    } while (*columnas < 2);
 }
 
+int main(void)
+{
 
-int main(void){
+    int filasA, columnasA, filasB, columnasB;
 
-    int filasA,columnasA,filasB,columnasB;
+    int **matrizA, **matrizB;
 
-    int **matrizA,**matrizB;
-
-    matrizA=asigancion_Memoria(&filasA,columnasB);
-    matrizB=asigancion_Memoria(&filasB,columnasB);
-
-
-
-
-
+    matrizA = asigancion_Memoria(&filasA, columnasA, 'A');
+    matrizB = asigancion_Memoria(&filasB, columnasB, 'B');
 
     return 0;
 }
