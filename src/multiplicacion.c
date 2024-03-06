f87894ccbab2c78a4a1ae6615653757dc9f7ea31
AlexMtzGzn <alex13141926@gmail.com>
2024-03-05 22:10:04 -0600

    Declaracion de la funcion asignar_Memoria

--- src/multiplicacion.c
+++ src/multiplicacion.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int** asigancion_Memoria(int * filas, int * columnas){
+
+    printf("\nIngresa las filas de la matriz %c: ");
+    printf("\nIngresa las columnas de la matriz %c: ");
+    
+}
 
 
 int main(void){
 
     int filasA,columnasA,filasB,columnasB;
+
+    int **matrizA,**matrizB;
+
+    matrizA=asigancion_Memoria(&filasA,columnasB);
+    matrizB=asigancion_Memoria(&filasB,columnasB);
 
 
 
